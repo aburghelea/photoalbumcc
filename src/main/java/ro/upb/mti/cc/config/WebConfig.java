7be20db598dfeec03038b1447674827a6e3d774e
Alexandru Burghelea <alexandru.burghelea@endava.com>
2013-12-18 12:51:52 +0200

    Added index.jsp as welcome file

--- src/main/java/ro/upb/mti/cc/config/WebConfig.java
+++ src/main/java/ro/upb/mti/cc/config/WebConfig.java
@@ -3,7 +3,10 @@ package ro.upb.mti.cc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * @author Alexandru Burghelea (alexandru.george.burghelea@gmail.com)
@@ -11,11 +14,18 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  */
 @EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
 
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setViewClass(JstlView.class);
         internalResourceViewResolver.setPrefix("/");
         internalResourceViewResolver.setSuffix(".jsp");
 
