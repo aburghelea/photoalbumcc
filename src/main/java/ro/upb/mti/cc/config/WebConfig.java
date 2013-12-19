1117fa06741f797b1947e5d08d411eb4d64d1f4a
Alexandru Burghelea <alexandru.burghelea@endava.com>
2013-12-19 12:34:00 +0200

    #4 Added webjar for twitter bootstrap (including jquery dep)

--- src/main/java/ro/upb/mti/cc/config/WebConfig.java
+++ src/main/java/ro/upb/mti/cc/config/WebConfig.java
@@ -22,6 +22,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Bean
